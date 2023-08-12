7852ccc0ed42ead84ea5b4ba556d34cc5b00147a
Sunku Sai Nisvas Sankarsh <sankarsh1905@gmail.com>
2023-08-12 10:18:01 +0530

    Modified UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,29 +27,25 @@ const addUser = asyncHandler(async (req, res) => {
   res.status(201).json(user);
 });
 
-//GET all Users
-const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.findAll();
-  if (_.isEmpty(users)) {
-    res.status(404).json({ message: "No Entries found." });
-  }
-  res.status(200).json(users);
-});
-
 //GET Users By Branch
 const getUserByBranch = asyncHandler(async (req, res) => {
-  const {userName,password,Branch} = req.body;
-  const condition = Branch && password && userName ? { userName: userName,password: password,Branch: Branch } : null;
+  const branch = req.params.branch ? req.params.branch : null;
+  if(branch==null){
+    res.status(400);
+    throw new Error("BranchName cannot be null");
+  }
+  const {userName,password} = req.body;
+  const condition = password && userName ? { userName: userName,password: password,Branch: branch } : null;
   if (condition === null) {
     res.status(400);
-    next(new Error("userName,password,BranchName cannot be null"));
+    next(new Error("userName,password cannot be null"));
   }
   const user = await User.findOne({ where: condition });
-  if (_.isEmpty(users)) {
+  if (user==null) {
     res.status(404);
     throw new Error("No Entries Found For The Given Branch.");
   }
   res.status(200).json({status : "SUCCESS", Role : user.Role});
 });
 
-module.exports = { addUser, getAllUsers, getUserByBranch };
+module.exports = { addUser, getUserByBranch };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,5 @@ const router = require("express").Router();
 //routes
 router.route("/").post(addUser);
 router.route("/:branch").get(getUserByBranch);
-router.route("/").get(getAllUsers);
 
 module.exports = router;
