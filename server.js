6b38937e2a3220a168565e20fed3a56e59648456
Sunku Sai Nisvas Sankarsh <sankarsh1905@gmail.com>
2023-08-13 12:23:09 +0530

    updated server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", require("./routes/routes"));
 app.use("/api/categories", require("./routes/categoryRoutes"));
 app.use("/api/user",require("./routes/userRoutes"));
+app.use("/api/customer",require("./routes/customerRoutes"));
 app.use(errorHandler);
 
 const port = process.env.PORT || 9001;
