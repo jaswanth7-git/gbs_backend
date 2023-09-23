69c199557a14c8230f7bbc90be7b29fac4387713
Sunku Sai Nisvas Sankarsh <sankarsh1905@gmail.com>
2023-09-23 21:11:50 +0530

    modified Advance Api

--- routes/advanceRoutes.js
+++ routes/advanceRoutes.js
@@ -10,6 +10,6 @@ const router = require("express").Router();
 router.route("/:phone").post(addAdvanceAmount);
 router.route("/:phone").get(getAdvanceAmountByCustomerNumber);
 router.route("/").get(getAllAdvanceAmounts);
-router.route("/:phone").put(updateAdvanceAmount)
+router.route("/update").put(updateAdvanceAmount)
 
 module.exports = router;
